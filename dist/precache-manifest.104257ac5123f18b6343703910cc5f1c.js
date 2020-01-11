6febfbf43c911e043cda3dda60ff1142ceb7b24a
janow <15036830368@163.com>
2020-01-11 23:14:03 +0800

    提交dist文件夹

---
+++ dist/precache-manifest.104257ac5123f18b6343703910cc5f1c.js
@@ -0,0 +1,21 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b1476450aa575e3ed310b215a0ae99",
+    "url": "fonts/本墨钧宋-e2b14.min.ttf"
+  },
+  {
+    "url": "images/4355da2694717a6b2f2b6dbcc93ca171.jpeg"
+  },
+  {
+    "revision": "0fa4a83e66a5bc92d171cd8863710c8b",
+    "url": "index.html"
+  },
+  {
+    "revision": "365869b8f7c3ff84f98b",
+    "url": "main.5177b9a33bfb82fe9806.js"
+  },
+  {
+    "revision": "365869b8f7c3ff84f98b",
+    "url": "main.css"
+  }
+]);
