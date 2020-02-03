431a45ed01bbbd38d3f1409e5b2e9a781e7eb214
janow <15036830368@163.com>
2020-02-03 20:46:46 +0800

    update

--- src/components/usePreviousValue.tsx
+++ src/components/usePreviousValue.tsx
@@ -1,10 +1,12 @@
-import React, { useRef, useEffect } from 'react'
+import { useRef, useEffect } from 'react'
 
 type strOrNum = number | string
 
 const usePreviousValue = (value: strOrNum) => {
+    // 增加一个长久引用的变量 ref 
     const ref = useRef() as any
 
+    // 在useEffect钩子里传递值
     useEffect(() => {
         ref.current = value
     })
